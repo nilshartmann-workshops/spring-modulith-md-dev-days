26c6980c25f8d40f43297a34a0c36a9580c9d4d2
Nils Hartmann <nils@nilshartmann.net>
2026-05-06 20:12:21 +0200

    04d: Explizite Abhängigkeiten

--- src/main/java/nh/demo/plantify/rose/RoseCareSuggestionFactory.java
+++ src/main/java/nh/demo/plantify/rose/RoseCareSuggestionFactory.java
@@ -1,5 +1,6 @@
 package nh.demo.plantify.rose;
 
+import nh.demo.plantify.care.CareTaskService;
 import nh.demo.plantify.care.suggestions.CareTaskSuggestion;
 import nh.demo.plantify.care.suggestions.CareTaskSuggestionFactory;
 import nh.demo.plantify.shared.PlantType;
@@ -10,7 +11,16 @@ import java.util.List;
 // Package private, keine öffentliche Komponente
 @Component
 class RoseCareSuggestionFactory implements CareTaskSuggestionFactory {
+//                                  ^--- IntelliJ Bug, wird weiterhin als Fehler angezeigt, Test ist grün
 //                                                    ^--- Problem: internes API
+    
+    private CareTaskService careTaskService;
+//                      ^--- Korrekt: CTS ist zwar public,
+//                           aber nicht Bestandteil des NamedInterfaces
+//                           das wir als einzige erlaubte Abhängigkeit
+//                           eingetragen haben --> Test!
+
+
     @Override
     public List<CareTaskSuggestion> createSuggestion(PlantType plantType, String location) {
 //                      ^--- IntelliJ Bug, wird weiterhin als Fehler angezeigt, Test ist grün
